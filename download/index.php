665607e5bb1475852084bb5f0ae0216e4fdd3505
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-01-05 16:32:52 +0100

    ~ made index dynamic

---
+++ download/index.php
@@ -0,0 +1,14 @@
+---
+layout: default_full
+breadcrumb: Download
+---
+
+{% include base.html %}
+
+<h1><b>Slizaa</b> releases</h1>
+
+<div class="content">
+<?php
+ include("versions.php");
+?>
+</div>
